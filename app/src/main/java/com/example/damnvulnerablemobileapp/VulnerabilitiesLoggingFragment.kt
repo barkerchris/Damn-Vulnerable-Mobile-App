3d8e6d870eeee1b49734942a71db0ae632ffa908
Chris Barker <chris.10697@gmail.com>
2021-03-10 14:14:43 +0000

    removed old data handling vulnerability

--- app/src/main/java/com/example/damnvulnerablemobileapp/VulnerabilitiesLoggingFragment.kt
+++ app/src/main/java/com/example/damnvulnerablemobileapp/VulnerabilitiesLoggingFragment.kt
@@ -19,6 +19,7 @@ class VulnerabilitiesLoggingFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View {
         _binding = FragmentVulnerabilitiesLoggingBinding.inflate(inflater, container, false)
+
         return binding.root
     }
 
@@ -37,16 +38,14 @@ class VulnerabilitiesLoggingFragment : Fragment() {
 
     private fun dumpLogs() {
         val fileName = "logs.txt"
-        val logFile = File(requireContext().cacheDir, fileName)
+        val logFile = File(requireContext().filesDir, fileName)
         try {
             val logcat = Runtime.getRuntime().exec("logcat -d")
             val reader = BufferedReader(InputStreamReader(logcat.inputStream))
             val writer = BufferedWriter(FileWriter(logFile))
             while (true) {
-                val line = reader.readLine()
-                if (line == null) {
-                   break
-                }
+                //If null then break
+                val line = reader.readLine() ?: break
                 writer.append(line).append('\n')
             }
             writer.flush()
